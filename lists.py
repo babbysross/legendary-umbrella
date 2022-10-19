57ba6b4632459d69645611d8143324a01913cedf
ross <ross@ambivist.com>
2022-10-19 22:36:41 +0100

    no

---
+++ lists.py
@@ -0,0 +1,5 @@
+list = ['cat', 'rat', 'bat', 'gat']
+print(list[0] + " with a " + list[3])
+list2 = [['cat', 'rat', 'bat', 'gat'],[10,20,30]]
+list2[0][2]='spat'
+print(list2[0][2] + " " + str(list2[1][:2]))
