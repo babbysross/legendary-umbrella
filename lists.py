b08f46355e77d13e3926dbda81f8605f9b1fc400
Ross Elias <rsg.elias@gmail.com>
2022-10-20 10:52:29 +0100

    finished lists activity

--- lists.py
+++ lists.py
@@ -1,5 +1,13 @@
+#ATBSWP Section 6-13
 list = ['cat', 'rat', 'bat', 'gat']
 print(list[0] + " with a " + list[3])
 list2 = [['cat', 'rat', 'bat', 'gat'],[10,20,30]]
 list2[0][2]='spat'
 print(list2[0][2] + " " + str(list2[1][:2]))
+print(list2[0][0:])
+print(len(list2))
+print('rat' in list2)
+del list2[0][1]
+print(list2[0][0:])
+print(len(list2))
+print('rat' not in list2)
