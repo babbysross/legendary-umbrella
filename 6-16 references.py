87df40af25adfb523b6b4bbca89f077f77477dcd
ross <ross@ambivist.com>
2022-10-21 21:14:17 +0100

    minor formatting changes

--- 6-16 references.py
+++ 6-16 references.py
@@ -1,14 +1,13 @@
 import copy
-                                    #example of how references are used differently between mutable (e.g. lists) and immutable (e.g strings, tuples) values
-
-def eggs(someparameter):            #define a function with a parameter (this will let you modify global variables that get passed in)
+                                    #example of how references are used differently between mutable (e.g. lists) and immutable (e.g strings, tuples) values.
+def eggs(someparameter):            #define a function with a parameter (this will let you modify global variables that get passed in).
     someparameter.append('Hello')
 
 spam = [1, 2, 3]
 eggs(spam)                          #feeding the spam array into the eggs function as the 'someparameter' parameter,
                                     #rather than copy the array into sameparameter, the function just references spam,
-                                    #so changes to sameparamer are changes to spam
-print(spam)                         #show that the append function adds to a variable outside its local scope
+                                    #so changes to sameparameter are changes to spam.
+print(spam)                         #show that the append function adds to a variable outside its local scope.
 cheese = copy.deepcopy(spam)        #deepcopy allows a proper copy of a mutable variable so each copy can be edited
                                     #separately.
 cheese.append(4)
